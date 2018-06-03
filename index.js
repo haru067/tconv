2e64eddc818350323c8dd8eee084a4efff3da103
haru067 <haru.pdf@gmail.com>
2018-06-03 15:54:41 +0900

    Japanese lock-in

--- index.js
+++ index.js
@@ -1,27 +1,57 @@
 #!/usr/bin/env node
 
 const { DateTime } = require('luxon');
-let argv = require('minimist')(process.argv.slice(2));
+const argv = require('minimist')(process.argv.slice(2));
 
 function parse(text) {
-    let dt = DateTime.fromISO(text);
-    if (dt.isValid) return toUnixtime(dt);
-    return toHumanReadableDate(text);
+    text = text.toString().replace(/\//g, '-').replace(/ /g, 'T'); // to ISO format
+    const dt = DateTime.fromISO(text);
+    if (dt.isValid) return unixtime(dt);
+    if (text && text.toString().length == 12) {
+        const src = text.toString();
+        const y = parseInt(src.substr(0, 4));
+        const m = parseInt(src.substr(4, 2));
+        const d = parseInt(src.substr(6, 2));
+        const h = parseInt(src.substr(8, 2));
+        const min = parseInt(src.substr(10, 2));
+        return unixtime(DateTime.local(y, m, d, h, min))
+    }
+
+    if (text && text.toString().length == 14) {
+        const src = text.toString();
+        const y = parseInt(src.substr(0, 4));
+        const m = parseInt(src.substr(4, 2));
+        const d = parseInt(src.substr(6, 2));
+        const h = parseInt(src.substr(8, 2));
+        const min = parseInt(src.substr(10, 2));
+        const s = parseInt(src.substr(12, 2));
+        return unixtime(DateTime.local(y, m, d, h, min, s))
+    }
+
+    return humanReadableDate(text);
 }
 
-function toUnixtime(dateTime) {
+function unixtime(dateTime) {
     return Math.floor(dateTime / 1000);
 }
 
-function toHumanReadableDate(text) {
+function humanReadableDate(text) {
     const timeMsec = parseInt(text) * 1000;
     const dt = DateTime.fromMillis(timeMsec);
-    return dt.toLocaleString(DateTime.DATETIME_HUGE_WITH_SECONDS);
+    const wd = getJapanseWeekday(dt.weekday);
+    const displayDate = `${dt.year}年${dt.month}月${dt.day}日（${wd}）${dt.hour}時${dt.minute}分${dt.second}秒`;
+    return displayDate;
+    // return dt.toLocaleString(DateTime.DATETIME_HUGE_WITH_SECONDS);
+}
+
+function getJapanseWeekday(weekday) {
+    const jp = ['?', '月', '火', '水', '木', '金', '土', '日'];
+    return jp[weekday] || '?';
 }
 
 if (argv._.length == 0) {
     // Unixtime: 1318781876
-    console.log(toUnixtime(DateTime.local()));
+    console.log(unixtime(DateTime.local()));
     return;
 }
 
--- test.js
+++ test.js
@@ -3,14 +3,17 @@ const baseCommand = 'node ./index.js';
 
 const test = (input, expected) => {
     const cmd = input ? `${baseCommand} ${input}` : baseCommand;
+    console.log('-------------------------');
     console.log(cmd);
     const result = execSync(cmd).toString().trim();
     if (result == expected) {
-        console.log(`[Success]`);
+        console.log(`[Success] ${input} -> ${expected}`);
     } else {
         console.log(`[Failed] result: ${result}, expected: ${expected}`);
     }
-    console.log(result);
 };
 
 test('20180102', 1514818800);
+test('201408111300', 1407729600);
+test('20170815071422', 1502748862);
+test("'2018/06/07 12:14'", 1528341240);
